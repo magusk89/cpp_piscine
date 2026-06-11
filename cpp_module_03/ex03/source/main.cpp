ba3cb6861bd9cea80baa051b453e082425cbb605
Alex Barbosa Felix Da Silva <alebarbo@c2r3s8.42porto.com>
2026-06-11 18:32:38 +0100

    fix: small changes

--- cpp_module_03/ex03/source/main.cpp
+++ cpp_module_03/ex03/source/main.cpp
@@ -6,7 +6,7 @@
 /*   By: alebarbo <alebarbo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/15 00:45:12 by alebarbo          #+#    #+#             */
-/*   Updated: 2026/06/11 18:24:14 by alebarbo         ###   ########.fr       */
+/*   Updated: 2026/06/11 18:31:35 by alebarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int main(void)
 {
 	DiamondTrap		diamonder("Diamonder");
 
+	std::cout << diamonder.getName() << " statistics: HP " << diamonder.getHp() << ", EP " << diamonder.getEp() << ", ATK " << diamonder.getAtk() << std::endl;
+	diamonder.attack("Coppie");
 	diamonder.whoAmI();
 
 	DiamondTrap		coppie(diamonder);
