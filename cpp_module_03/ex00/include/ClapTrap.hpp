a2b44fdd5d7caca7f7b9ebe864f701ff9e4cd5b5
Alex Barbosa Felix Da Silva <alebarbo@c2r3s8.42porto.com>
2026-06-11 14:48:30 +0100

    fix: changed several aspects of ex00

--- cpp_module_03/ex00/include/ClapTrap.hpp
+++ cpp_module_03/ex00/include/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: alebarbo <alebarbo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/15 00:46:02 by alebarbo          #+#    #+#             */
-/*   Updated: 2026/03/15 02:27:22 by alebarbo         ###   ########.fr       */
+/*   Updated: 2026/06/11 14:46:34 by alebarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,24 @@
 class ClapTrap
 {
 	private:
-		std::string		name;
-		int				hp;
-		int				ep;
-		int				atk;
+		static const int		_maxHp = 10;
+		static const int		_maxEp = 10;
+		static const int		_atkDmg = 0;
+
+		std::string		_name;
+		int				_hp;
+		int				_ep;
+		int				_atk;
 
 	public:
 						ClapTrap();
-						ClapTrap(std::string new_name);
+						ClapTrap(std::string name);
 						ClapTrap(const ClapTrap &copy);
 						~ClapTrap();
 		ClapTrap		&operator=(const ClapTrap &copy);
-		std::string		getName(void);
-		int				getHp(void);
-		int				getEp(void);
-		int				getAtk(void);
-		void			setName(std::string new_name);
-		void			setHp(int new_hp);
-		void			setEp(int new_ep);
-		void			setAtk(int new_atk);
+		void			setHp(const int hp);
+		void			setEp(const int ep);
+		void			setAtk(const int atk);
 		void			attack(const std::string &target);
 		void			takeDamage(unsigned int amount);
 		void			beRepaired(unsigned int amount);
--- cpp_module_03/ex00/source/ClapTrap.cpp
+++ cpp_module_03/ex00/source/ClapTrap.cpp
@@ -6,130 +6,104 @@
 /*   By: alebarbo <alebarbo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/15 00:45:41 by alebarbo          #+#    #+#             */
-/*   Updated: 2026/03/15 20:56:03 by alebarbo         ###   ########.fr       */
+/*   Updated: 2026/06/11 14:46:06 by alebarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ClapTrap.hpp>
 
-ClapTrap::ClapTrap() : name("ClapTrap"), hp(10), ep(10), atk(0)
+ClapTrap::ClapTrap() : _name("ClapTrap"), _hp(_maxHp), _ep(_maxEp), _atk(_atkDmg)
 {
-	std::cout << "Default constructor called." << std::endl;
+	std::cout << "A ClapTrap defaults into the arena!" << std::endl;
 }
 
-ClapTrap::ClapTrap(std::string new_name) : name(new_name), hp(10), ep(10), atk(0)
+ClapTrap::ClapTrap(std::string name) : _name(name), _hp(_maxHp), _ep(_maxEp), _atk(_atkDmg)
 {
-	std::cout << this->getName() << " parametized constructor called." << std::endl;
+	std::cout << _name << " parametizes into the arena!" << std::endl;
 }
 
-ClapTrap::ClapTrap(const ClapTrap &copy)
+ClapTrap::ClapTrap(const ClapTrap &copy) : _name(copy._name), _hp(copy._hp), _ep(copy._ep), _atk(copy._atk)
 {
-	std::cout << "Copy constructor called, copying " << copy.name << std::endl;
-	*this = copy;
+	std::cout << "A ClapTrap entered the arena, copying " << copy._name << "!" << std::endl;
 }
 
 ClapTrap::~ClapTrap()
 {
-	std::cout << this->getName() << " destructor called." << std::endl;
+	std::cout << _name << " was smashed to pieces!" << std::endl;
 }
 
 ClapTrap &ClapTrap::operator=(const ClapTrap &copy)
 {
-	std::cout << this->name << " copied from " << copy.name << std::endl;
+	std::cout << _name << " is mimicking " << copy._name << std::endl;
 	if (&copy != this)
 	{
-		this->setName(copy.name);
-		this->setHp(copy.hp);
-		this->setEp(copy.ep);
-		this->setAtk(copy.atk);
+		_name = copy._name;
+		_hp = copy._hp;
+		_ep = copy._ep;
+		_atk = copy._atk;
 	}
 	return (*this);
 }
 
-std::string ClapTrap::getName(void)
+void ClapTrap::setHp(const int hp)
 {
-	return (this->name);
+	_hp = hp;
 }
 
-int ClapTrap::getHp(void)
+void ClapTrap::setEp(const int ep)
 {
-	return (this->hp);
+	_ep = ep;
 }
 
-int ClapTrap::getEp(void)
+void ClapTrap::setAtk(const int atk)
 {
-	return (this->ep);
-}
-
-int ClapTrap::getAtk(void)
-{
-	return (this->atk);
-}
-
-void ClapTrap::setName(std::string new_name)
-{
-	this->name = new_name;
-}
-
-void ClapTrap::setHp(int new_hp)
-{
-	this->hp = new_hp;
-}
-
-void ClapTrap::setEp(int new_ep)
-{
-	this->ep = new_ep;
-}
-
-void ClapTrap::setAtk(int new_atk)
-{
-	this->atk = new_atk;
+	_atk = atk;
 }
 
 void ClapTrap::attack(const std::string &target)
 {
-	if (this->hp <= 0)
-		std::cout << "ClapTrap \x1b[1;32m" << this->name << "\x1b[0m cannot attack \x1b[1;31m" << target << "\x1b[0m because it is dead." << std::endl;
-	else if (this->ep > 0)
+	if (_hp <= 0)
+		std::cout << "ClapTrap \x1b[1;32m" << _name << "\x1b[0m cannot attack \x1b[1;31m" << target << "\x1b[0m because it is dead." << std::endl;
+	else if (_ep > 0)
 	{
-		this->ep--;
-		std::cout << "ClapTrap \x1b[1;32m" << this->name << "\x1b[0m attacks \x1b[1;31m" << target << "\x1b[0m, causing \x1b[1;31m" << this->atk << "\x1b[0m points of damage!" << std::endl;
-		std::cout << "ClapTrap \x1b[1;32m" << this->name << "\x1b[0m has \x1b[1;33m" << this->ep << "\x1b[0m energy points left!" << std::endl;
+		_ep--;
+		std::cout << "ClapTrap \x1b[1;32m" << _name << "\x1b[0m attacks \x1b[1;31m" << target << "\x1b[0m, causing \x1b[1;31m" << _atk << "\x1b[0m points of damage!" << std::endl;
+		std::cout << "ClapTrap \x1b[1;32m" << _name << "\x1b[0m has \x1b[1;33m" << _ep << "\x1b[0m energy points left!" << std::endl;
 	}
 	else
-		std::cout << "ClapTrap \x1b[1;32m" << this->name << "\x1b[0m cannot attack \x1b[1;31m" << target << "\x1b[0m because it has no energy points!" << std::endl;
+		std::cout << "ClapTrap \x1b[1;32m" << _name << "\x1b[0m cannot attack \x1b[1;31m" << target << "\x1b[0m because it has no energy points!" << std::endl;
 	std::cout << std::endl;
 }
 
 void ClapTrap::takeDamage(unsigned int amount)
 {
-	if (this->hp <= 0)
-		std::cout << "Stop, stop! ClapTrap \x1b[1;32m" << this->name << "\x1b[0m is already dead!" << std::endl;
+	if (_hp <= 0)
+		std::cout << "Stop, stop! ClapTrap \x1b[1;32m" << _name << "\x1b[0m is already dead!" << std::endl;
 	else
 	{
-		std::cout << "ClapTrap \x1b[1;32m" << this->name << "\x1b[0m takes \x1b[1;31m" << amount << "\x1b[0m points of damage!" << std::endl;
-		this->hp -= amount;
-		if (this->hp <= 0)
-			std::cout << "ClapTrap \x1b[1;32m" << this->name << "\x1b[0m has died!" << std::endl;
+		std::cout << "ClapTrap \x1b[1;32m" << _name << "\x1b[0m takes \x1b[1;31m" << amount << "\x1b[0m points of damage!" << std::endl;
+		_hp -= amount;
+		if (_hp <= 0)
+			std::cout << "ClapTrap \x1b[1;32m" << _name << "\x1b[0m has died!" << std::endl;
 		else
-			std::cout << "ClapTrap \x1b[1;32m" << this->name << "\x1b[0m has \x1b[1;35m" << this->hp << "\x1b[0m hit points left!" << std::endl;
+			std::cout << "ClapTrap \x1b[1;32m" << _name << "\x1b[0m has \x1b[1;35m" << _hp << "\x1b[0m hit points left!" << std::endl;
 	}
 	std::cout << std::endl;
 }
 
 void ClapTrap::beRepaired(unsigned int amount)
 {
-	if (this->hp <= 0)
-		std::cout << "ClapTrap \x1b[1;32m" << this->name << "\x1b[0m cannot repair itself because it is dead." << std::endl;
-	else if (this->ep > 0)
+	if (_hp <= 0)
+		std::cout << "ClapTrap \x1b[1;32m" << _name << "\x1b[0m cannot repair itself because it is dead." << std::endl;
+	else if (_ep > 0)
 	{
-		this->ep--;
-		this->hp += amount;
-		std::cout << "ClapTrap \x1b[1;32m" << this->name << "\x1b[0m repairs itself, recovering \x1b[1;35m" << amount << "\x1b[0m hit points!" << std::endl;
-		std::cout << "ClapTrap \x1b[1;32m" << this->name << "\x1b[0m has \x1b[1;35m" << this->hp << "\x1b[0m hit points left!" << std::endl;
-		std::cout << "ClapTrap \x1b[1;32m" << this->name << "\x1b[0m has \x1b[1;33m" << this->ep << "\x1b[0m energy points left!" << std::endl;
+		_ep--;
+		_hp += amount;
+		std::cout << "ClapTrap \x1b[1;32m" << _name << "\x1b[0m repairs itself, recovering \x1b[1;35m" << amount << "\x1b[0m hit points!" << std::endl;
+		std::cout << "ClapTrap \x1b[1;32m" << _name << "\x1b[0m has \x1b[1;35m" << _hp << "\x1b[0m hit points left!" << std::endl;
+		std::cout << "ClapTrap \x1b[1;32m" << _name << "\x1b[0m has \x1b[1;33m" << _ep << "\x1b[0m energy points left!" << std::endl;
 	}
 	else
-		std::cout << "ClapTrap \x1b[1;32m" << this->name << "\x1b[0m cannot be repaired because it has no energy points!" << std::endl;
+		std::cout << "ClapTrap \x1b[1;32m" << _name << "\x1b[0m cannot be repaired because it has no energy points!" << std::endl;
 	std::cout << std::endl;
 }
